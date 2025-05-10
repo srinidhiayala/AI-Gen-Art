7fb24d594dd4838417e70a28aa7ac8cc16cbf5c7
srinidhiayala <145507362+srinidhiayala@users.noreply.github.com>
2025-05-10 15:04:18 -0400

    redesign done

--- static/routing.js
+++ static/routing.js
@@ -7,9 +7,29 @@ $(document).ready(function () {
     window.location.href = '/learning/1';
   });
 
+  $('#begin-quizzes').on('click', function () {
+    window.location.href = '/big_quiz/1';
+  });
+
   $('#beginQuiz-button').on('click', function () {
     window.location.href = '/big_quiz/1';
   });  
+
+  $('#redo-learning').on('click', function () {
+    window.location.href = '/learning/1';
+  });  
+
+  $('.js-return-home').click(function () {
+    window.location.href = '/';
+  });
+
+  $('.js-retake-quiz').click(function () {
+    window.location.href = '/big_quiz/1';
+  });
+
+  $('.js-redo-learning').click(function () {
+    window.location.href = '/learning/1';
+  });
 
   $('#next-button').click(function () {
     const nextId = $(this).data('next-id');
@@ -24,11 +44,9 @@ $(document).ready(function () {
       window.location.href = `/quiz/${backId.substring(1)}`;
     }
   });
+
   $('#back-bq-button').click(function () {
-    const backId = $(this).data('back-id');
-    if (backId && backId !== "null") {
-      window.location.href = `/quiz/${backId.substring(1)}`;
-    }
+    window.location.href = `/quiz/5`;
   });
   $('#back-mod-button').click(function () {
     const backId = $(this).data('back-id');
